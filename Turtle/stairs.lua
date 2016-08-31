067511651efe6faacbc289fee05d29491ad7701d
michmerr <michmerr@MICHMERRLAP>
2016-08-30 18:28:31 -0700

    Fixed some obvious issues with intervalActions, and added one for sealing stairwells.

--- Turtle/stairs.lua
+++ Turtle/stairs.lua
@@ -235,7 +235,7 @@ end
 function runIntervalActions(job)
   if intervalActions then
     for _, action in pairs(job.intervalActions) do
-      action(job)
+      action.check(job)
     end
   end
 end
@@ -382,12 +382,20 @@ function placeTorches(horizontalInterval, verticalInterval, height, materialSlot
 
     return result
   end
+
+  return action
 end
 
 function placeRailings(materialSlotRangeLow, materialSlotRangeHigh, materialTypes)
 
-  return function(job)
-    if above or job.isOnWall() then
+  local action = { }
+
+  action.materialSlotRangeLow = materialSlotRangeLow
+  action.materialSlotRangeHigh = materialSlotRangeHigh
+  action.materialTypes = materialTypes
+
+  function action.check(job)
+    if job.above or job.isOnWall() then
       return true
     end
 
@@ -398,13 +406,84 @@ function placeRailings(materialSlotRangeLow, materialSlotRangeHigh, materialType
       turtle.forward()
       placeDown(job.materialSlotRangeLow, job.materialSlotRangeHigh, job.materialTypes)
       turtle.back()
-      result = placeForward(materialSlotRangeLow, materialSlotRangeHigh, materialTypes)
+      result = placeForward(action.materialSlotRangeLow, action.materialSlotRangeHigh, action.materialTypes)
     end
 
     turtle.turnTo(originalFacing)
     return result
   end
+
+  return action
 end
 
+function seal(materialSlotRangeLow, materialSlotRangeHigh, materialTypes)
+
+  local action = { }
+
+  action.materialSlotRangeLow = materialSlotRangeLow
+  action.materialSlotRangeHigh = materialSlotRangeHigh
+  action.materialTypes = materialType
+
+  function action.check(job)
+    local result = true
+    local originalFacing = turtle.getFacing()
+    if job.isOnWall() then
+      turtle.turnTo(job.towardsWall)
+    else
+      turtle.turnTo(job.towardsEdge)
+    end
+
+    if job.above then
+      turtle.down()
+      if not turtle.detect() and not placeForward(action.materialSlotRangeLow, action.materialSlotRangeHigh, action.materialTypes) then
+        turtle.down()
+        if not placeForward(action.materialSlotRangeLow, action.materialSlotRangeHigh, action.materialTypes) then
+          turtle.forward()
+          if not turtle.placeDown(action.materialSlotRangeLow, action.materialSlotRangeHigh, action.materialTypes) then
+            local loc = turtle.getLocation()
+            print(string.format("error sealing stairwell at %d, %d, %d (relative)", loc.x, loc.y, loc.z))
+          end
+          turtle.back()
+          result = placeForward(action.materialSlotRangeLow, action.materialSlotRangeHigh, action.materialTypes) and result
+        end
+        turtle.up()
+        result = placeForward(action.materialSlotRangeLow, action.materialSlotRangeHigh, action.materialTypes) and result
+      end
+      turtle.up()
+      result = turtle.detect() or placeForward(action.materialSlotRangeLow, action.materialSlotRangeHigh, action.materialTypes) and result
+      turtle.up()
+      result = turtle.detect() or placeForward(action.materialSlotRangeLow, action.materialSlotRangeHigh, action.materialTypes) and result
+      if not turtle.detectUp() and not placeUp(action.materialSlotRangeLow, action.materialSlotRangeHigh, action.materialTypes) then
+        turtle.up()
+        placeForward(action.materialSlotRangeLow, action.materialSlotRangeHigh, action.materialTypes)
+        turtle.down()
+        placeUp(action.materialSlotRangeLow, action.materialSlotRangeHigh, action.materialTypes)
+      end
+      local halfWay = math.ceil(job.treadWidth / 2)
+      for i = 2, halfWay do
+        turtle.back()
+        placeUp(action.materialSlotRangeLow, action.materialSlotRangeHigh, action.materialTypes)
+      end
+      for i = 2, halfWay do
+        turtle.forward()
+      end
+      turtle.down()
+    else
+      if not turtle.detect() and not placeForward(action.materialSlotRangeLow, action.materialSlotRangeHigh, action.materialTypes) then
+        turtle.forward()
+        if not turtle.placeDown(action.materialSlotRangeLow, action.materialSlotRangeHigh, action.materialTypes) then
+          local loc = turtle.getLocation()
+          print(string.format("error sealing stairwell at %d, %d, %d (relative)", loc.x, loc.y, loc.z))
+        end
+        turtle.back()
+        result = placeForward(action.materialSlotRangeLow, action.materialSlotRangeHigh, action.materialTypes) and result
+      end
+    end
+    turtle.turnTo(originalFacing)
+    return result
+  end
+
+  return action
+end
 
 -- endregion
